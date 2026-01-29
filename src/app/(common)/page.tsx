a6989e08f845f13bad698b3a8649e4c0f1a5af5e
Mohammad-Anar <ai.anarul4232@gmail.com>
2026-01-30 03:03:15 +0600

    dashbaord page cards added

--- src/app/(common)/page.tsx
+++ src/app/(common)/page.tsx
@@ -10,11 +10,17 @@ const page = () => {
       <div className="flex flex-1 flex-col">
         <div className="@container/main flex flex-1 flex-col gap-2">
           <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <div className="p-5">
+              <h3 className="text-2xl sm:text-3xl  font-semibold mb-3 text-primary/90">
+                Dashboard Overview
+              </h3>
+              <p>Welcome back, Admin</p>
+            </div>
             <SectionCards />
-            <div className="px-4 lg:px-6">
+            {/* <div className="px-4 lg:px-6">
               <ChartAreaInteractive />
             </div>
-            <DataTable data={data} />
+            <DataTable data={data} /> */}
           </div>
         </div>
       </div>
--- src/components/section-cards.tsx
+++ src/components/section-cards.tsx
@@ -1,6 +1,13 @@
-import { IconTrendingDown, IconTrendingUp } from "@tabler/icons-react"
+import {
+  IconClock,
+  IconPaw,
+  IconShoppingCart,
+  IconTrendingDown,
+  IconTrendingUp,
+  IconUsersGroup,
+} from "@tabler/icons-react";
 
-import { Badge } from "@/components/ui/badge"
+import { Badge } from "@/components/ui/badge";
 import {
   Card,
   CardAction,
@@ -8,95 +15,82 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
+import { ArrowDown, ArrowUp, Minus, Plus } from "lucide-react";
+import { Span } from "next/dist/trace";
 
 export function SectionCards() {
+  const data = [
+    {
+      title: "Total Animals",
+      amount: 2874,
+      percentage: 12,
+      text: "from last month",
+      icon: <IconPaw size={30} className="text-purple-600" />,
+    },
+    {
+      title: "Total Orders",
+      amount: 1234,
+      percentage: 8,
+      text: "from last month",
+      icon: <IconShoppingCart size={30} className="text-purple-600" />,
+    },
+    {
+      title: "Pending Orders",
+      amount: 89,
+      percentage: -5,
+      text: "from yesterday",
+      icon: <IconClock size={30} className="text-purple-600" />,
+    },
+    {
+      title: "Users Count",
+      amount: 15672,
+      percentage: 25,
+      text: "from last month",
+      icon: <IconUsersGroup size={30} className="text-purple-600" />,
+    },
+  ];
   return (
-    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Total Revenue</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            $1,250.00
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +12.5%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Trending up this month <IconTrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">
-            Visitors for the last 6 months
-          </div>
-        </CardFooter>
-      </Card>
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>New Customers</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            1,234
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingDown />
-              -20%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Down 20% this period <IconTrendingDown className="size-4" />
-          </div>
-          <div className="text-muted-foreground">
-            Acquisition needs attention
-          </div>
-        </CardFooter>
-      </Card>
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Active Accounts</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            45,678
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +12.5%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Strong user retention <IconTrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">Engagement exceed targets</div>
-        </CardFooter>
-      </Card>
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Growth Rate</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            4.5%
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +4.5%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Steady performance increase <IconTrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">Meets growth projections</div>
-        </CardFooter>
-      </Card>
+    <div className="*:data-[slot=card]:from-white *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+      {data.map((item, index) => (
+        <Card key={index} className="@container/card border-2">
+          <CardHeader>
+            <CardDescription>{item.title}</CardDescription>
+            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+              ${item.amount}
+            </CardTitle>
+            <CardAction>
+              <div className="w-10 h-10 bg-purple-200 flex items-center justify-center rounded-md p-2">
+                {item.icon}
+              </div>
+            </CardAction>
+          </CardHeader>
+          <CardFooter className="flex-col items-start gap-1.5 text-sm">
+            <div
+              className={` line-clamp-1 flex gap-2 font-medium flex items-center ${item.percentage < 0 ? "text-my-red" : "text-my-green"}`}
+            >
+              {item.percentage > 0 ? (
+                <span className="text-my-green">
+                  <ArrowUp size={16} />
+                </span>
+              ) : (
+                <span className="text-my-red">
+                  <ArrowDown size={16} />
+                </span>
+              )}
+              {item.percentage > 0 ? (
+                <span className="flex items-center text-my-green">
+                  {<Plus size={10} />}
+                  {String(item.percentage).concat("%")}
+                </span>
+              ) : (
+                <span>{String(item.percentage).concat("%")}</span>
+              )}
+              {item.text}
+            </div>
+          </CardFooter>
+        </Card>
+      ))}
     </div>
-  )
+  );
 }
